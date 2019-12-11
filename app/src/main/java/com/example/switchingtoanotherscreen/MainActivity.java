3bddb089eca6b9623e24c118da05d5425766e115
Emil9 <emil.grabowski@interia.pl>
2019-12-11 17:30:37 +0100

    wersja 2

---
+++ app/src/main/java/com/example/switchingtoanotherscreen/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.switchingtoanotherscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openScreen2();
+            }
+        });
+    }
+
+    public void openScreen2() {
+        Intent intent = new Intent(this, Screen2.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/switchingtoanotherscreen/Screen2.java
@@ -0,0 +1,33 @@
+package com.example.switchingtoanotherscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Screen2 extends AppCompatActivity {
+
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen2);
+
+        button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openScreen1();
+            }
+        });
+    }
+
+    public void openScreen1() {
+        Intent intent2 = new Intent(this, MainActivity.class);
+        startActivity(intent2);
+    }
+}
